bbb3cc925b34649e7860ca7b12cba0c3c3aac288
刘士辰 <liushichen@liushichendeMacBook-Pro.local>
2025-01-09 11:41:41 +0800

    feat: 🎸 模版添加更换projectName

--- packages/create/src/index.ts
+++ packages/create/src/index.ts
@@ -1,9 +1,11 @@
-import { select, input } from "@inquirer/prompts";
+import { select, input, confirm } from "@inquirer/prompts";
 import os from "node:os";
 import { NpmPackage } from "@ollie-cli/utils";
 import path from "node:path";
 import ora from "ora";
 import fse from "fs-extra";
+import { glob } from "glob";
+import ejs from "ejs";
 async function create() {
   const projectTemplate = await select({
     message: "请选择项目模版",
@@ -23,7 +25,16 @@ async function create() {
   while (!projectName) {
     projectName = await input({ message: "请输入项目名" });
   }
+  const targetPath = path.join(process.cwd(), projectName);
 
+  if (fse.existsSync(targetPath)) {
+    const empty = await confirm({ message: "该目录不为空，是否清空" });
+    if (empty) {
+      fse.emptyDirSync(targetPath);
+    } else {
+      process.exit(0);
+    }
+  }
   const pkg = new NpmPackage({
     name: projectTemplate,
     targetPath: path.join(os.homedir(), ".ollie-cli-template"),
@@ -45,9 +56,22 @@ async function create() {
   await sleep(1000);
 
   const templatePath = path.join(pkg.npmFilePath, "template");
-  const targetPath = path.join(process.cwd(), projectName);
 
   fse.copySync(templatePath, targetPath);
+
+  const files = await glob("**", {
+    cwd: targetPath,
+    nodir: true,
+    ignore: "node_modules/**",
+  });
+
+  for (let i = 0; i < files.length; i++) {
+    const filePath = path.join(targetPath, files[i]);
+    const renderResult = await ejs.renderFile(filePath, {
+      projectName,
+    });
+    fse.writeFileSync(filePath, renderResult);
+  }
 
   spinner.stop();
 }
